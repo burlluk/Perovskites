8a65ee96c8ddda6602bb5f1b9fc0b789cd3cc4a5
Daniel Alborta <alborta@wisc.edu>
2017-04-09 23:56:48 -0500

    Options in Comments

--- NNTrainingScript.m
+++ NNTrainingScript.m
@@ -1,10 +1,13 @@
+numInputs = 1;
+%inputs = {(0:pi/64:6.28); (0:pi/63.5:2*pi)};
 inputs = (0:pi/64:2*pi);
+%targets = sin(inputs{1})+2*sin(inputs{2});
 targets = sin(inputs);
 allRMSE = zeros(0, 5000);
 allTestTargets = zeros(0, 10000);
 PredOutputs = zeros(0, 10000);
 double RMSE;
-numRuns = 10;
+numRuns = 100;
 numBins = 30;
 num = 0;
 trainPct = 1;
@@ -24,6 +27,7 @@ fprintf(fileID, '%s\n', 'Standard Deviation of all sin(x): ', std(targets));
 fprintf(fileID, '%s\n', 'Mean of all sin(x): ', mean(targets));
 %Writes the R^2 value
 mdl = fitlm(inputs, targets);
+%fprintf(fileID, '%s\n', 'Rsquared for sin(x): ', mdl.Rsquared.Adjusted);
 fprintf(fileID, '%s\n', 'Rsquared for sin(x): ', mdl.Rsquared.Ordinary);
 %Writes the Std Dev of sin(x)
 fprintf(fileID, '%s\n', 'Standard Deviation of all sin(x): ', std(targets));
@@ -35,6 +39,7 @@ DispHistogram(targets, 50, 'Histogram of all sin(x) values', 'Value', 'Occurence
 normalTars= (targets-mean(targets))/std(targets);
 %disp('Normalized Targets');
 
+%normalIns= {(inputs{1}-mean(inputs{1}))/std(inputs{1}); (inputs{2}-mean(inputs{2}))/std(inputs{2})};
 normalIns= (inputs-mean(inputs))/std(inputs);
 %disp('Normalized Inputs');
 
@@ -49,7 +54,7 @@ for j=0:3
     RMSESum = 0;
     for i=0:numRuns-1
         %Calls function to set parameters as desired
-        network = netParams(trainPct, testPct, valPct, 10);
+        network = netParams(trainPct, testPct, valPct, numInputs, 10);
 
         %Hides the NNtraintool window for "faster" training
         network.trainParam.showWindow = false;
@@ -124,9 +129,11 @@ for j=0:3
     if (num ~= 0)
     %Smallest R^2
     mdl2 = fitlm(minRMSEPred, minRMSETargets);
+    %fprintf(fileID, '%s\n', 'Smallest R^2: ', mdl2.Rsquared.Adjusted);
     fprintf(fileID, '%s\n', 'Smallest R^2: ', mdl2.Rsquared.Ordinary);
     %Largest R^2
     mdl3 = fitlm(maxRMSEPred, maxRMSETargets);
+    %fprintf(fileID, '%s\n', 'Largest R^2: ', mdl3.Rsquared.Adjusted);
     fprintf(fileID, '%s\n', 'Largest R^2: ', mdl3.Rsquared.Ordinary);
     end
 
